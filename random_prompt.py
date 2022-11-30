b763f26fd9e20d015e7d81af4420a41a48d145bb
Hui <huiyao8761380@gmail.com>
2022-11-30 13:10:12 +0800

    Update random_prompt.py

--- random_prompt.py
+++ random_prompt.py
@@ -9,65 +9,84 @@ from modules.processing import process_images, StableDiffusionProcessingTxt2Img
 
 class Script(scripts.Script):
     def title(self):
-        return "random prompt 0.1"
+        return "random prompt 0.5"
 
     def ui(self, is_img2img):
-        dummy = gr.Textbox(label="random prompt script has been started",value="tag example:,<short hair|long hair|messy hair>;   Batch count>=1,Batch size=1")
-        #sametag = gr.Checkbox(label="Same tag can be generated.", value=False)
-        #norand = gr.Checkbox(label="Not random,do each prompt", value=False)
+        dummy = gr.Textbox(label="random prompt script has been started",value="tag example:,<short|long|messy> hair;   Batch count>=1,Batch size=1")
         return [dummy]
 
     def run(self, p, dummy):
-        #modules.processing.fix_seed(p)
 
         original_prompt = p.prompt[0] if type(p.prompt) == list else p.prompt
 
         all_prompts = [original_prompt]
-        #p_prompt=all_prompts[0]
         #之前只能叠4层到10w
 
-        #p.prompt[0].
-        ####datafinish=False
+        split_str=['']
+        right_str=[]
+        gen_prompt=''
+        new_prompt=''
+        
         for this_prompt in all_prompts:
             for data in re.finditer(r'(<([^>]+)>)', this_prompt):
                 if data:
-                    ####datafinish=True
                     span = data.span(1)
                     new_prompt = this_prompt[:span[0]]
                     gen_prompt = this_prompt[span[0]:]
+                    this_str=gen_prompt.replace("<",">")
+                    split_str =this_str.split(">")
                     break
         
         #print(f"new_prompt：{new_prompt}")
         #print(f"gen_prompt：{gen_prompt}")
         
-        ####if datafinish==True:
-        #for i_prompt in all_prompts:
+        
+        split_str[0]=split_str[0].strip()
+        split_str[0]=split_str[0].strip(',')
+        split_str[0]=split_str[0]+' '
+        for in_str in split_str:
+            if "|" not in in_str:
+                if "," in in_str:
+                    FL_str=in_str.split(",")
+                    right_str.append(FL_str[0])
+                    right_str.append(FL_str[1])
+                else:
+                    right_str.append(in_str)
+        #print (right_str)
+        #print (len(right_str))
+        #d=int((len(right_str)-1)/2)
+        #print(d)
+        
         for ip in range(p.n_iter+1):
             
             my_prompt=""
-            #all_prompts.remove(i_prompt)
+            #first_prompt=gen_prompt.split("<")
+            i=0
             for data in re.finditer(r'(<([^>]+)>)', gen_prompt):
                 if data:
-                    
+
                     items = data.group(2).split("|")#=list
                     length=len(items)-1
+
                     rand_item = items[random.randint(0,length)]
-                    my_prompt=my_prompt+rand_item.strip()+","
+                    
+                    rand_str=right_str[i]+rand_item+right_str[i+1]
+                    
+                    my_prompt=my_prompt+rand_str.strip()+","
+                    i=i+2
+                    
             i_prompt=new_prompt+my_prompt
             all_prompts.append(i_prompt)
-            print(f"i_prompt：{i_prompt}")
-            print(f"all_prompts len：{len(all_prompts)}")
- 
-        all_prompts.remove(all_prompts[0])
+            #print(f"i_prompt：{i_prompt}")
+            #print(f"all_prompts len：{len(all_prompts)}")
+        if gen_prompt != '':
+            all_prompts.remove(all_prompts[0])
 
 
         
         
 
         p.prompt = all_prompts * p.n_iter #all_prompts * p.n_iter
-        #else:
-        #p.prompt = out_prompts*p.n_iter
-        #p.n_iter = total_images
         #print(f"p.n_iter：{p.n_iter}") #=batch_count
         p.do_not_save_grid = True
         p.prompt_for_display = original_prompt
